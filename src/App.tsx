cb9d011e540005b60249192795a857c84be2830d
Tomer Pearl <tomerp20@gmail.com>
2023-11-14 14:14:51 +0200

    removing unnecessary line

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ function App() {
         <Route path='' element={<Home/>}/>
         <Route path='' element={<Home/>}/>
         <Route path='' element={<Home/>}/>
-        <Route path='' element={<Home/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
