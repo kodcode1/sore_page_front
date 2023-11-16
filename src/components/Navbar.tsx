7886c16229070aae916a21ea8ee3f193c23fbae1
Shay Arbes <shayarbes1@gmail.com>
2023-11-16 09:30:22 +0200

    fix bug footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import { AppBar, Box, Toolbar, IconButton, Typography, MenuItem, Button } from "@mui/material";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { Link, useNavigate } from "react-router-dom";
-import HomeIcon from "@mui/icons-material/Home";
-export default function Navbar() {
-  const navigate = useNavigate();
-=======
 import { AppBar, Box, Toolbar, IconButton, Typography, MenuItem, Button, Badge } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Link, useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
+import PlaceIcon from '@mui/icons-material/Place';
 import HomeIcon from "@mui/icons-material/Home";
 export default function Navbar() {
   const navigate = useNavigate();
@@ -27,7 +19,6 @@ export default function Navbar() {
   //   console.log(cart.length);
  
   // }, []);
->>>>>>> main
   return (
     <Box sx={{ flexGrow: 1, minHeight: "0px" }}>
       <AppBar position="static" sx={{ background: "white" }}>
@@ -37,15 +28,13 @@ export default function Navbar() {
           </Typography>
 
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-<<<<<<< HEAD
-            <IconButton size="large" color="primary" onClick={() => navigate("/")}>
-              <ShoppingCartIcon />
-=======
             <IconButton size="large" color="primary" onClick={() => navigate("/cart")}>
             <Badge badgeContent={1} color="error">
             <ShoppingCartIcon />
         </Badge>
->>>>>>> main
+            </IconButton>
+            <IconButton size="large" color="primary" onClick={() => navigate("/")}>
+              <PlaceIcon />
             </IconButton>
             <IconButton size="large" color="primary" onClick={() => navigate("/")}>
               <HomeIcon />
